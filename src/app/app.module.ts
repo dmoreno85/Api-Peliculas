d5218f8c59253b4795c79a15a3f1c86708ab91eb
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-06-26 17:47:55 +0200

    cambios varios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
-=======
 import { FooterComponent } from './footer/footer.component';
-<<<<<<< HEAD
->>>>>>> master
-=======
 import { ContentPrincipalComponent } from './content-principal/content-principal.component';
->>>>>>> master
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HeaderComponent
-=======
     HeaderComponent,
-<<<<<<< HEAD
-    FooterComponent
->>>>>>> master
-=======
     FooterComponent,
     ContentPrincipalComponent
->>>>>>> master
   ],
   imports: [
     BrowserModule,
