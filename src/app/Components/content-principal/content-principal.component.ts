f4d4d626d24304925d8812a7f8907bffe8b16958
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-06-27 16:49:36 +0200

    juntado el componente de lola. Creado el header con submenus desplegables

--- src/app/Components/content-principal/content-principal.component.ts
+++ src/app/Components/content-principal/content-principal.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-content-principal',
   templateUrl: './content-principal.component.html',
   styleUrls: ['./content-principal.component.scss']
 })
-export class ContentPrincipalComponent implements OnInit {
+export class ContentPrincipalComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
