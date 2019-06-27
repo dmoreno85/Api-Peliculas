b1645f79647d280a2244782c1ca4293210624c4a
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-06-27 09:30:20 +0200

    cambiada estructura de proyecto. Creada vista Index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentPrincipalComponent } from './content-principal/content-principal.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ContentPrincipalComponent } from './Components/content-principal/content-principal.component';
+import { IndexComponent } from './Views/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentPrincipalComponent
+    ContentPrincipalComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
