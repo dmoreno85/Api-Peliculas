41b085cf8a435a5cb8b444aca6f8e9c2b83bc6a0
Lola <lall91069@gmail.com>
2019-06-28 17:14:51 +0200

    prueba1

--- src/app/peliculas.service.ts
+++ src/app/peliculas.service.ts
@@ -12,7 +12,5 @@ export class PeliculasService {//generamos el servicio con ng g service [nombre 
   getEstrenosPeliculas():Observable<any>{
     return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=57100bbbe8d760beada498e98fb84066&language=en-US&page=1');
   }
-
-  // getPeliculas2017():Obs
 }
 
