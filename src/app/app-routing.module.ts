4e8f0faaefc8c3875e3a276be958ba2b6a59e906
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-06-28 10:15:03 +0200

    routing creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { IndexComponent } from './Views/index/index.component';
+import { ResultsComponent } from './Views/results/results.component';
+import { PelisComponent } from './Views/pelis/pelis.component';
+import { SeriesComponent } from './Views/series/series.component';
+
+const routes: Routes = [
+  { path: "", component: IndexComponent },
+  { path: "results", component: ResultsComponent },
+  { path: "pelis", component: PelisComponent},
+  { path: "series", component: SeriesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
