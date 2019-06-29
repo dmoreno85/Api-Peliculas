cfb7f9423d142f66fb922de9434ff828bbfeb51f
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-06-29 18:14:36 +0200

    peliculas hechas

--- src/app/Components/films-results/films-results.component.ts
+++ src/app/Components/films-results/films-results.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { PeliculasService } from '../../peliculas.service';
 
 @Component({
   selector: 'app-films-results',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmsResultsComponent implements OnInit {
 
-  constructor() { }
+  private peliculasPopulares: Object[]
+  private page: Number = 1;
+  private totalPages: Number;
+  private languaje: String = "en-US";
+
+  constructor(private peliculasService: PeliculasService) { }
 
   ngOnInit() {
+    this.peliculasService.getTodasPeliculas(this.page, this.languaje).subscribe(value => {
+      console.log(value);
+      this.page = value.page;
+      this.totalPages = value.total_pages;
+      this.peliculasPopulares = value.results;
+    }, error => console.log(error));
+  }
+
+  nextPage() {
+    if (this.page < this.totalPages) {
+      this.page++;
+      this.peliculasService.getTodasPeliculas(this.page, this.languaje).subscribe(value => {
+        this.peliculasPopulares = value.results;
+      }, error => console.log('No se han podido recuperar los datos'))
+      console.log(this.page);
+    }
+  }
+
+  previousPage() {
+    if (this.page > 1) {
+      this.page--;
+      this.peliculasService.getTodasPeliculas(this.page, this.languaje).subscribe(value => {
+        this.peliculasPopulares = value.results;
+      }, error => console.log('No se han podido recuperar los datos'));
+      console.log(this.page);
+    }
   }
 
 }
