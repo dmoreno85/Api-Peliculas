c6f8063b668eb941ce2b39dc1708c6396d5c930e
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-06-29 14:09:24 +0200

    componente films creado

---
+++ src/app/Components/films-results/films-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-films-results',
+  templateUrl: './films-results.component.html',
+  styleUrls: ['./films-results.component.scss']
+})
+export class FilmsResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
