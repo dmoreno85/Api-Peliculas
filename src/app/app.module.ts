4c458772032f2c6ea4439fe368e5175b35adf340
Cesar Ocampo <cesarocampo9@hotmail.com>
2019-07-01 10:49:47 +0200

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
-import { HomeComponent } from './home/home.component';
 import { ComponenteSecundarioComponent } from './componente-secundario/componente-secundario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent,
-    HomeComponent,
+   
     ComponenteSecundarioComponent
   ],
   imports: [
---
+++ src/app/peliculas.service.ts
@@ -0,0 +1,16 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable ({
+//   providedIn: 'root',
+// })
+// export class PeliculasService {
+//   getPeliculasService(page: number) {
+//     throw new Error("Method not implemented.");
+//   }
+//   getPopularPeliculas(arg0: number) {
+//     throw new Error("Method not implemented.");
+//   }
+  
+
+//   constructor() { }
+// }
