a28e1b2fd31faed726434e29bb56711058040efc
Cesar Ocampo <cesarocampo9@hotmail.com>
2019-07-02 15:26:50 +0200

    cambios en conten-secundario

--- src/app/Components/content-secundario/content-secundario.component.ts
+++ src/app/Components/content-secundario/content-secundario.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PeliculasService} from  '../../peliculas.service';
 @Component({
   selector: 'app-content-secundario',
   templateUrl: './content-secundario.component.html',
   styleUrls: ['./content-secundario.component.scss']
 })
-export class ContentSecundarioComponent implements OnInit {
 
-  constructor() { }
+export class ContentSecundarioComponent implements OnInit {
+  private peliculas:Object[]
+  year:number;
+  constructor(private peliculasService: PeliculasService) { }//aquí inyecto el servicio PeliculasService al componente upcoming-movies)
 
   ngOnInit() {
   }
+  filtrarPorMejores(year:number):void{
+    console.log(year)
+    this.peliculasService.getPeliculasByYear(year).subscribe(res => this.peliculas=res.results.splice(0,5), error=>console.log(error))
+  }
+  filtrarPorMejoresIntervalo(desde:number,hasta:number):void{
 
+  }
 }
