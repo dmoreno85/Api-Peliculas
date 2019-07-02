0964e800e47ca3fffd8a44c018d8cb2dbacee1d1
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-07-02 16:02:14 +0200

    filtro solucionado

--- src/app/Components/films-results/films-results.component.ts
+++ src/app/Components/films-results/films-results.component.ts
@@ -96,20 +96,20 @@ export class FilmsResultsComponent implements OnInit, DoCheck {
     if (this.peliculas) {
       if (order === 'nameAscendant') {
         this.peliculas.sort(function (a, b) {
-          if (a.title > b.title) {
+          if (a['title'] > b['title']) {
             return 1;
           }
-          if (a.title < b.title) {
+          if (a['title'] < b['title']) {
             return -1;
           }
           return 0;
         });
       } else {
         this.peliculas.sort(function (a, b) {
-          if (a.title < b.title) {
+          if (a['title'] < b['title']) {
             return 1;
           }
-          if (a.title > b.title) {
+          if (a['title'] > b['title']) {
             return -1;
           }
           return 0;
@@ -123,12 +123,24 @@ export class FilmsResultsComponent implements OnInit, DoCheck {
     console.log(this.orderBy);
     if (this.peliculas) {
       if (order === 'dateAscendant') {
-        this.peliculas.sort(function (a, b) {
-          return new Date(a.release_date) - new Date(b.release_date);
+        this.peliculas.sort((a:Object, b:Object) => {
+          if (a['release_date'] < b['release_date']) {
+            return 1;
+          }
+          if (a['release_date'] > b['release_date']) {
+            return -1;
+          }
+          return 0;
         });
       } else {
-        this.peliculas.sort(function (a, b) {
-          return new Date(b.release_date) - new Date(a.release_date);
+        this.peliculas.sort((a:Object, b:Object) => {
+          if (a['release_date'] > b['release_date']) {
+            return 1;
+          }
+          if (a['release_date'] < b['release_date']) {
+            return -1;
+          }
+          return 0;
         });
       }
     }
@@ -141,20 +153,20 @@ export class FilmsResultsComponent implements OnInit, DoCheck {
       if (this.orderBy === 'popularityAscendant') {
 
         this.peliculas.sort(function (a, b) {
-          if (a.popularity < b.popularity) {
+          if (a['popularity'] < b['popularity']) {
             return 1;
           }
-          if (a.popularity > b.popularity) {
+          if (a['popularity'] > b['popularity']) {
             return -1;
           }
           return 0;
         });
       } else {
         this.peliculas.sort(function (a, b) {
-          if (a.popularity > b.popularity) {
+          if (a['popularity'] > b['popularity']) {
             return 1;
           }
-          if (a.popularity < b.popularity) {
+          if (a['popularity'] < b['popularity']) {
             return -1;
           }
           return 0;
@@ -165,7 +177,7 @@ export class FilmsResultsComponent implements OnInit, DoCheck {
 
   getGenres() {
     let allGenres = this.peliculasService.getAllGenres().subscribe(value => {
-      this.genres = value.genres;
+      this.genres = value['genres'];
     }, error => console.log(error));
   }
 
