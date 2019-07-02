041deff0048529cb5e5d3d41221ec7b7618d7ad4
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-07-02 12:37:20 +0200

    filtros de las peliculas realizados

--- src/app/Components/films-results/films-results.component.ts
+++ src/app/Components/films-results/films-results.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck } from '@angular/core';
 import { PeliculasService } from '../../peliculas.service';
 
 import { ActivatedRoute } from '@angular/router';
-
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-films-results',
   templateUrl: './films-results.component.html',
   styleUrls: ['./films-results.component.scss']
 })
-export class FilmsResultsComponent implements OnInit {
+export class FilmsResultsComponent implements OnInit, DoCheck {
 
+  private pageTitle: string;
+  private orderBy: string;
   private resultType: any;
-  private peliculasPopulares: Object[]
+  private peliculas: Object[]
   private page: number = 1;
   private totalPages: number;
   private languaje: string = "en-US";
@@ -31,16 +31,46 @@ export class FilmsResultsComponent implements OnInit {
         console.log(value);
         this.page = value.page;
         this.totalPages = value.total_pages;
-        this.peliculasPopulares = value.results;
+        this.peliculas = value.results;
+
+        switch (this.resultType) {
+          case "all":
+            return this.pageTitle = 'Todas las películas';
+          case "populares":
+            return this.pageTitle = 'Películas populares';
+          case "novedades":
+            return this.pageTitle = 'Películas de estreno';
+          case "mejor-puntuadas":
+            return this.pageTitle = 'Películas mejor puntuadas';
+          default:
+            return this.pageTitle = 'Películas';
+        }
       }, error => console.log(error));
     }, error => console.log(error));
+  }
+
+  ngDoCheck() {
+    switch (this.orderBy) {
+      case "nameAscendant":
+      case "nameDescendant":
+        this.byName(this.orderBy);
+        break;
+      case "dateAscendant":
+      case "dateDescendant":
+        this.byTime(this.orderBy);
+        break;
+      case "popularityAscendant":
+      case "popularityDescendant":
+        this.byPopulation(this.orderBy);
+        break;
+    }
   }
 
   nextPage() {
     if (this.page < this.totalPages) {
       this.page++;
       this.peliculasService.getTodasPeliculas(this.page, this.languaje, this.resultType).subscribe(value => {
-        this.peliculasPopulares = value.results;
+        this.peliculas = value.results;
       }, error => console.log('No se han podido recuperar los datos'))
       console.log(this.page);
     }
@@ -50,9 +80,83 @@ export class FilmsResultsComponent implements OnInit {
     if (this.page > 1) {
       this.page--;
       this.peliculasService.getTodasPeliculas(this.page, this.languaje, this.resultType).subscribe(value => {
-        this.peliculasPopulares = value.results;
+        this.peliculas = value.results;
       }, error => console.log('No se han podido recuperar los datos'));
       console.log(this.page);
+    }
+  }
+
+
+
+  byName(order: String) {
+    this.orderBy = order;
+    if (this.peliculas) {
+      if (order === 'nameAscendant') {
+        this.peliculas.sort(function (a, b) {
+          if (a.title > b.title) {
+            return 1;
+          }
+          if (a.title < b.title) {
+            return -1;
+          }
+          return 0;
+        });
+      } else {
+        this.peliculas.sort(function (a, b) {
+          if (a.title < b.title) {
+            return 1;
+          }
+          if (a.title > b.title) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+    }
+  }
+
+  byTime(order: String) {
+    this.orderBy = order;
+    console.log(this.orderBy);
+    if (this.peliculas) {
+      if (order === 'dateAscendant') {
+        this.peliculas.sort(function (a, b) {
+          return new Date(a.release_date) - new Date(b.release_date);
+        });
+      } else {
+        this.peliculas.sort(function (a, b) {
+          return new Date(b.release_date) - new Date(a.release_date);
+        });
+      }
+    }
+  }
+
+  byPopulation(order: String) {
+    this.orderBy = order;
+    console.log(this.orderBy);
+    if (this.peliculas) {
+      if (this.orderBy === 'popularityAscendant') {
+
+        this.peliculas.sort(function (a, b) {
+          if (a.popularity < b.popularity) {
+            return 1;
+          }
+          if (a.popularity > b.popularity) {
+            return -1;
+          }
+          return 0;
+        });
+      } else {
+        this.peliculas.sort(function (a, b) {
+          if (a.popularity > b.popularity) {
+            return 1;
+          }
+          if (a.popularity < b.popularity) {
+            return -1;
+          }
+          return 0;
+        });
+      }
     }
   }
 
