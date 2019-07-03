7a9b7fcc373c6bf0f59a611c54e9ff074d3c0c2b
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-07-03 13:07:41 +0200

    cambios de jose añadidos 2

--- src/app/peliculas.service.ts
+++ src/app/peliculas.service.ts
@@ -27,7 +27,7 @@ export class PeliculasService {//generamos el servicio con ng g service [nombre 
     }
   };
 
-  getAllGenres() {
+  getAllGenres():Observable<any>{
     return this.http.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=57100bbbe8d760beada498e98fb84066&language=en-US`)
   }
 
@@ -40,7 +40,7 @@ export class PeliculasService {//generamos el servicio con ng g service [nombre 
 
   }
 
-  getPeliByGenre(genre:number, page:Number){
+  getPeliByGenre(genre:number, page:Number):Observable<any>{
     return this.http.get(`https://api.themoviedb.org/3/discover/movie?with_genres=${genre}&api_key=57100bbbe8d760beada498e98fb84066&page=${page}`)
   }
 }
