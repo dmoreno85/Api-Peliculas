e4eed72364abb1823389359a0148e5baa24ee033
David <dmoreno788@gmail.com>
2019-07-03 10:03:58 +0200

    Añadido node-vibrant, a falta de prueba.

--- src/app/Components/series-info/series-info.component.ts
+++ src/app/Components/series-info/series-info.component.ts
@@ -2,20 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { SeriesService } from '../../series.service'
 import { ActivatedRoute } from '@angular/router'
 
+
+
 @Component({
   selector: 'app-series-info',
   templateUrl: './series-info.component.html',
   styleUrls: ['./series-info.component.scss']
 })
 export class SeriesInfoComponent implements OnInit {
-
+  private background:string;
   private id: string;
-  private serieInfo: Object;
+  private serieInfo: any;
   private language: string = "es-ES"
-
+  private obj:any;
   constructor(
     private seriesService: SeriesService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    
   ) { }
 
   ngOnInit() {
@@ -23,6 +26,9 @@ export class SeriesInfoComponent implements OnInit {
     this.seriesService.getInfoSerie(this.id, this.language).subscribe(value => {
       this.serieInfo=value;
       console.log(this.serieInfo)
+  
+     
+      this.obj ={'background-image':`linear-gradient(#f1c40fa5, #f1c40fd4), url('https://image.tmdb.org/t/p/w780${this.serieInfo.backdrop_path}')`}
     },error=>console.log(error))
   }
 
