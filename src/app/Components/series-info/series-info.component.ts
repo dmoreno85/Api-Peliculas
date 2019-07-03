edcf85865bbbb9f03579ad5efb73f72ca8765c1e
David <dmoreno788@gmail.com>
2019-07-03 11:11:41 +0200

    puesto el color predominante de la imagen de fondo. Commit by Jose :)

--- src/app/Components/series-info/series-info.component.ts
+++ src/app/Components/series-info/series-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SeriesService } from '../../series.service'
 import { ActivatedRoute } from '@angular/router'
+import Vibrant from 'node-vibrant'
+import { Palette } from 'node-vibrant/lib/color'
 
 
 
@@ -10,26 +12,53 @@ import { ActivatedRoute } from '@angular/router'
   styleUrls: ['./series-info.component.scss']
 })
 export class SeriesInfoComponent implements OnInit {
-  private background:string;
+  private background: string;
   private id: string;
   private serieInfo: any;
   private language: string = "es-ES"
-  private obj:any;
+
+  private transparencia: string = "d4"
+
+  private palette: Palette;
+  private obj: any;
   constructor(
     private seriesService: SeriesService,
     private route: ActivatedRoute,
-    
+
   ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get("id");
     this.seriesService.getInfoSerie(this.id, this.language).subscribe(value => {
-      this.serieInfo=value;
+      this.serieInfo = value;
       console.log(this.serieInfo)
-  
-     
-      this.obj ={'background-image':`linear-gradient(#f1c40fa5, #f1c40fd4), url('https://image.tmdb.org/t/p/w780${this.serieInfo.backdrop_path}')`}
-    },error=>console.log(error))
+      let img = `https://image.tmdb.org/t/p/w780/${this.serieInfo.backdrop_path}`
+      Vibrant.from(img).getPalette((err, palette) => {
+        console.log(palette)
+        console.log(err)
+        this.palette = palette;
+      });
+
+    }, error => console.log(error))
+
+
+  }
+
+  styleContainer(): any {
+    console.log('palette', this.palette);
+    if (this.palette.LightVibrant) {
+
+
+      return {
+        'background-image': `linear-gradient(${this.palette.LightVibrant.getHex()}${this.transparencia},${this.palette.LightVibrant.getHex()}${this.transparencia}), url('https://image.tmdb.org/t/p/w780${this.serieInfo.backdrop_path}')`
+      }
+    } else {
+
+      return {
+        'background-image': `linear-gradient(${this.palette.LightMuted.getHex()}${this.transparencia},${this.palette.LightMuted.getHex()}${this.transparencia}), url('https://image.tmdb.org/t/p/w780${this.serieInfo.backdrop_path}')`
+      };
+    }
+
   }
 
 }
