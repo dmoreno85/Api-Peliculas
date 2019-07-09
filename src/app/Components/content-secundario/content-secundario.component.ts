7cbad67b22d0f6af0423a176a23a7faae9414c02
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-07-09 15:15:33 +0200

    todo juntado y solucionado

--- src/app/Components/content-secundario/content-secundario.component.ts
+++ src/app/Components/content-secundario/content-secundario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { PeliculasService} from  '../../peliculas.service';
+import { PeliculasService } from '../../peliculas.service';
 
 @Component({
   selector: 'app-content-secundario',
@@ -9,46 +9,31 @@ import { PeliculasService} from  '../../peliculas.service';
 
 
 export class ContentSecundarioComponent implements OnInit {
-<<<<<<< HEAD
-  private peliculas:object[];
+  private peliculas: object[];
+  private series: Object[];
   private id: string;
-  year:number;
-    constructor(
-      private peliculasService: PeliculasService
-             ) { }//aquí inyecto el servicio PeliculasService al componente upcoming-movies)
-ngDoCheck(){
-}
-  ngOnInit() {
-    }
-=======
+  year: number;
+  principalLoaded: boolean = false;
+  constructor(private peliculasService: PeliculasService) { }//aquí inyecto el servicio PeliculasService al componente upcoming-movies)
+
   ngOnInit(): void {
-    
-  }
-  private peliculas:Object[]
-  private series:Object[]
-  year:number;
-    constructor(private peliculasService: PeliculasService , private SeriesService:SeriesService) { }//aquí inyecto el servicio PeliculasService al componente upcoming-movies)
-    principalLoaded:boolean=false;
 
->>>>>>> 5285e2fcea2ef969d5ec90ca0e249e915e10071b
-  filtrarPorMejores(year:number):void{
-    this.peliculasService.getPeliculasByYear(year).subscribe(res => this.peliculas=res.results.splice(0,5), error=>console.log(error))
   }
-  
 
-  filtrarPorMejoresIntervalo(desde:number,hasta:number):void{
+  filtrarPorMejores(year: number): void {
+    this.peliculasService.getPeliculasByYear(year).subscribe(res => this.peliculas = res.results.splice(0, 5), error => console.log(error))
+  }
+
+
+  filtrarPorMejoresIntervalo(desde: number, hasta: number): void {
     console.log(this.year)
-    this.peliculasService.getPeliculasByYearI(this.year,this.year).subscribe(res => this.peliculas=res.results.splice(0,5), error=>console.log(error))
+    this.peliculasService.getPeliculasByYearI(this.year, this.year).subscribe(res => this.peliculas = res.results.splice(0, 5), error => console.log(error))
   }
-<<<<<<< HEAD
-  
-  
-=======
-  ngDoCheck(){
-    this.principalLoaded=this.peliculasService.principalLoaded;
+
+  ngDoCheck() {
+    this.principalLoaded = this.peliculasService.principalLoaded;
   }
-  ngOnDestroy(){
-    this.peliculasService.principalLoaded=false;
+  ngOnDestroy() {
+    this.peliculasService.principalLoaded = false;
   }
->>>>>>> 5285e2fcea2ef969d5ec90ca0e249e915e10071b
 }
