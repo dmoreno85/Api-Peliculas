277618f3606d308141837da1f9a997bdad373734
Lola <lall91069@gmail.com>
2019-07-09 17:40:46 +0200

    credits y button en peliInfo

--- src/app/peliculas.service.ts
+++ src/app/peliculas.service.ts
@@ -35,6 +35,10 @@ export class PeliculasService {//generamos el servicio con ng g service [nombre 
     return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=57100bbbe8d760beada498e98fb84066`);
   }
 
+  getCreditsPeli(id: String, language: String): Observable<Object> {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=57100bbbe8d760beada498e98fb84066&language=${language}`);
+  };
+
   getTrailersPelis(id: String, language: String): Observable<any> {
     return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=57100bbbe8d760beada498e98fb84066&language=${language}`);
   };
