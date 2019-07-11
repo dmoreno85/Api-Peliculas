f9c0e016cfe48e8e1a1550857c26ffbb2e4f1655
David <dmoreno788@gmail.com>
2019-07-11 17:32:54 +0200

    Heroku resuelto

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/movies-angular'));
+app.use(express.static(__dirname + '/dist/Api-Peliculas'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/movies-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Api-Peliculas/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
